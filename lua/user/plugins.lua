cf96a4c7cd3d507af83bcb7dcef0b91d196ee6a2
= <puneet@Puneet.localdomain>
2022-12-17 12:39:43 +0530

    Added neoscroll

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,4 +1,6 @@
 lvim.plugins = {
   { 'tanvirtin/monokai.nvim' },
-  { 'karb94/neoscroll.nvim' }
+  { 'karb94/neoscroll.nvim' },
+  { 'Puneet56/puneet-nvim' },
+  { 'bluz71/vim-nightfly-colors' }
 }
